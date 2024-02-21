7d44716c1996e5084ad827db69af17a48a5cee9f
I k <ilona.koschi@hotmail.com>
2024-02-21 15:52:21 +0100

    added svg code and functions

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ function render() {
             let cellValue = '';
 
             if (fieldValue === 'circle') {
-                cellValue = 'O';
+                cellValue = generateCircleSVG();
             } else if (fieldValue === 'cross') {
-                cellValue = 'X';
+                cellValue = generateCrossSVG();
             }
 
             tableHTML += `<td>${cellValue}</td>`;
@@ -33,3 +33,39 @@ function render() {
 
     document.getElementById('content').innerHTML = tableHTML;
 }
+
+function generateCircleSVG() {
+    const radius = 35; // Radius des Kreises
+    const strokeWidth = 8; // Die Dicke der Linie
+    const viewBoxPadding = strokeWidth; // Zusätzlicher Platz in der viewBox für den Strich
+    const viewBoxSize = radius * 2 + viewBoxPadding * 2; // Gesamtgröße der viewBox unter Berücksichtigung des Strichs
+    const circlePosition = radius + viewBoxPadding; // Neue Position des Kreises innerhalb der viewBox
+    const circumference = 2 * Math.PI * radius; // Umfang des Kreises berechnen
+
+    const svgHTML = `
+    <svg width="70" height="70" viewBox="0 0 ${viewBoxSize} ${viewBoxSize}" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="${circlePosition}" cy="${circlePosition}" r="${radius}" stroke="#00B0EF" stroke-width="${strokeWidth}" fill="none" stroke-dasharray="${circumference}" stroke-dashoffset="${circumference}">
+            <animate attributeName="stroke-dashoffset" from="${circumference}" to="0" dur="1s" fill="freeze" />
+        </circle>
+    </svg>
+    `;
+
+    return svgHTML;
+}
+
+
+function generateCrossSVG() {
+    const svgHTML = `
+    <svg width="70" height="70" viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg">
+        <path d="M5,5 L65,65" stroke="#FFC000" stroke-width="8" fill="none">
+            <animate attributeName="stroke-dasharray" from="0,90,0" to="45,0,45" dur="1s" fill="freeze" />
+        </path>
+        <path d="M65,5 L5,65" stroke="#FFC000" stroke-width="8" fill="none">
+            <animate attributeName="stroke-dasharray" from="0,90,0" to="45,0,45" dur="1s" fill="freeze" />
+        </path>
+    </svg>
+    `;
+
+    return svgHTML;
+}
+
